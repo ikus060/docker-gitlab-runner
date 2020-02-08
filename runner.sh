f171234a2e4fd0b9b386abb6304b95e1155bb6c2
Patrik Dufresne <ikus060@gmail.com>
2020-02-07 21:28:50 -0500

    Allow reusable of Token
    Reuse the same token if possible to reduce the number of registered runner.

--- runner.sh
+++ runner.sh
@@ -1,53 +1,48 @@
 #!/usr/bin/env bash
-set -x
 set -e
+set -x
 
-pid=0
-token=()
-
-# SIGTERM-handler
-term_handler() {
-  if [ $pid -ne 0 ]; then
-    kill -SIGTERM "$pid"
-    wait "$pid"
-  fi
-  gitlab-runner unregister -u ${GITLAB_URL} -t ${token}
-  exit 143; # 128 + 15 -- SIGTERM
-}
+if [ -z $GITLAB_URL ]; then
+    echo "GITLAB_URL is required" 
+    exit 1
+fi
+if [ -z $GITLAB_RUNNER_TOKEN ]; then
+    echo "GITLAB_RUNNER_TOKEN is required"
+    exit 1
+fi
 
 # Wait until gitlab is responding
-until curl -s ${GITLAB_URL}; do
-    sleep 2
+until curl -s ${GITLAB_URL}>/dev/null; do
+    echo "Wait for gitlab ${GITLAB_URL} to become available..."
+    sleep 5
 done
 sleep 5
 
-# register runner
-gitlab-runner register --non-interactive \
-                       --url ${GITLAB_URL} \
-                       --registration-token ${GITLAB_RUNNER_TOKEN} \
-                       --executor docker \
-                       --name "${GITLAB_RUNNER_NAME:-runner}" \
-                       --output-limit "20480" \
-                       --docker-image "docker:latest" \
-                       --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
-		       ${GITLAB_REGISTER_ARGS}
-
-# setup handlers
-# on callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
-trap 'kill ${!}; term_handler' SIGTERM
-
-unset GITLAB_RUNNER_TOKEN
-# Update concurrent value
-sed -i "s/concurrent.*/concurrent = ${GITLAB_CONCURRENT:=1}/" /etc/gitlab-runner/config.toml
-
-# assign runner token
-token=$(cat /etc/gitlab-runner/config.toml | grep token | awk '{print $3}' | tr -d '"')
+# register runner only if not configure.
+# when user mount /etc/gitlab-runner, the config may already exists.
+if [ ! -z "$CI_SERVER_TOKEN" ]; then
+    echo "Use provided runner token from CI_SERVER_TOKEN"
+elif [ -e /etc/gitlab-runner/config.toml ]; then
+    echo "Re-use existing runner token from /etc/gitlab-runner/config.toml"
+    export CI_SERVER_TOKEN=$(cat /etc/gitlab-runner/config.toml | grep token | awk '{print $3}' | tr -d '"')
+else
+    echo "Register new runner"
+    gitlab-runner register \
+        --non-interactive \
+        --url ${GITLAB_URL} \
+        --registration-token "$GITLAB_RUNNER_TOKEN" \
+        --name "${GITLAB_RUNNER_NAME:-runner}" \
+        ${GITLAB_REGISTER_ARGS}
+    export CI_SERVER_TOKEN=$(cat /etc/gitlab-runner/config.toml | grep token | awk '{print $3}' | tr -d '"')
+fi
 
 # run multi-runner
-gitlab-ci-multi-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner & pid="$!"
-
-# wait forever
-while true
-do
-  tail -f /dev/null & wait ${!}
-done
+gitlab-runner run-single \
+    --url ${GITLAB_URL} \
+    --name "${GITLAB_RUNNER_NAME:-runner}"
+    --executor docker \
+    --output-limit "20480" \
+    --docker-image "docker:latest" \
+    --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+    --limit ${GITLAB_CONCURRENT:=1} \
+    ${GITLAB_REGISTER_ARGS}
