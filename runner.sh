d982e71cc8aadc2d0e695193d6fb68e525f18dfe
Patrik Dufresne <ikus060@gmail.com>
2020-02-07 22:10:52 -0500

    Minor fix for reuse token

--- runner.sh
+++ runner.sh
@@ -20,29 +20,30 @@ sleep 5
 
 # register runner only if not configure.
 # when user mount /etc/gitlab-runner, the config may already exists.
-if [ ! -z "$CI_SERVER_TOKEN" ]; then
-    echo "Use provided runner token from CI_SERVER_TOKEN"
-elif [ -e /etc/gitlab-runner/config.toml ]; then
+if [ -e /etc/gitlab-runner/config.toml ]; then
     echo "Re-use existing runner token from /etc/gitlab-runner/config.toml"
-    export CI_SERVER_TOKEN=$(cat /etc/gitlab-runner/config.toml | grep token | awk '{print $3}' | tr -d '"')
 else
     echo "Register new runner"
     gitlab-runner register \
         --non-interactive \
         --url ${GITLAB_URL} \
-        --registration-token "$GITLAB_RUNNER_TOKEN" \
+        --registration-token ${GITLAB_RUNNER_TOKEN} \
+        --executor docker \
         --name "${GITLAB_RUNNER_NAME:-runner}" \
+        --output-limit "20480" \
+        --docker-image "docker:latest" \
+        --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
         ${GITLAB_REGISTER_ARGS}
-    export CI_SERVER_TOKEN=$(cat /etc/gitlab-runner/config.toml | grep token | awk '{print $3}' | tr -d '"')
 fi
 
+# Update concurrent value
+sed -i "s/concurrent.*/concurrent = ${GITLAB_CONCURRENT:=1}/" /etc/gitlab-runner/config.toml
+
 # run multi-runner
-gitlab-runner run-single \
-    --url ${GITLAB_URL} \
-    --name "${GITLAB_RUNNER_NAME:-runner}"
-    --executor docker \
-    --output-limit "20480" \
-    --docker-image "docker:latest" \
-    --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
-    --limit ${GITLAB_CONCURRENT:=1} \
-    ${GITLAB_REGISTER_ARGS}
+gitlab-runner start
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
